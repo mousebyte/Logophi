2b871175b266787aca4f2569d519f22f165cd757
mousebyte <ateague063@gmail.com>
2019-02-06 15:15:54 -0500

    Document IViewPresenter.cs

--- Views/IViewPresenter.cs
+++ Views/IViewPresenter.cs
@@ -3,18 +3,39 @@
 namespace MouseNet.Logophi.Views
 {
     
-    
+    /// <inheritdoc />
+    /// <summary>
+    /// Presents an <see cref="T:MouseNet.Logophi.Views.IView" />.
+    /// </summary>
+    /// <typeparam name="TView">The type of view.</typeparam>
     public interface IViewPresenter<TView> : IDisposable
     {
+        /// <summary>
+        /// The view currently being presented.
+        /// </summary>
         TView View { get; }
+        
+        /// <summary>
+        /// Presents a <see cref="TView"/>.
+        /// </summary>
+        /// <param name="view">The view to present.</param>
         void Present
             (TView view);
 
+        /// <summary>
+        /// Presents a <see cref="TView"/> using the given object as
+        /// its parent.
+        /// </summary>
+        /// <param name="view">The view to present.</param>
+        /// <param name="parent">The parent to use for the view.</param>
         void Present
             (TView view,
              object parent);
     }
 
+    /// <summary>
+    /// Provides data for the <see cref="IView.ViewEventActivated"/> event.
+    /// </summary>
     public class ViewEventArgs : EventArgs
     {
         internal ViewEventArgs
@@ -23,6 +44,9 @@ namespace MouseNet.Logophi.Views
             Tag = tag;
             }
         
+        /// <summary>
+        /// The data tag associated with the event.
+        /// </summary>
         public object Tag { get; }
     }
 }
