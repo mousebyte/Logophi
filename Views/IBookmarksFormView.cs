19f7f019a65919d634864d2844dcd83b44d3614d
mousebyte <ateague063@gmail.com>
2019-02-06 23:07:39 -0500

    Document IBookmarksFormView.cs

--- Views/IBookmarksFormView.cs
+++ Views/IBookmarksFormView.cs
@@ -3,10 +3,23 @@ using System.Collections;
 
 namespace MouseNet.Logophi.Views
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Exposes a bookmarks form view.
+    /// </summary>
     public interface IBookmarksFormView : IView
     {
+        /// <summary>
+        /// Gets the list of bookmarks displayed in the view.
+        /// </summary>
         IList Items { get; }
+        /// <summary>
+        /// Occurs when the view is closed.
+        /// </summary>
         event EventHandler Closed;
+        /// <summary>
+        /// Occurs when a bookmark is removed.
+        /// </summary>
         event EventHandler<string> BookmarkRemoved;
     }
 }
