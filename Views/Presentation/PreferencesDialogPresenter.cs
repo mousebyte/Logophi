8da435c49c326c0d9462225c16aff71c3c452125
mousebyte <ateague063@gmail.com>
2019-02-08 14:56:00 -0500

    Document PreferencesDialogPresenter.cs

--- Views/Presentation/PreferencesDialogPresenter.cs
+++ Views/Presentation/PreferencesDialogPresenter.cs
@@ -4,19 +4,28 @@ using MouseNet.Logophi.Properties;
 
 namespace MouseNet.Logophi.Views.Presentation
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Presents an <see cref="T:MouseNet.Logophi.Views.IPreferencesDialogView" />.
+    /// </summary>
     internal class PreferencesDialogPresenter
         : IViewPresenter<IPreferencesDialogView>
     {
         private IPreferencesDialogView _view;
         public IPreferencesDialogView View => _view;
+        /// <summary>
+        /// Gets a value indicating whether or not the view is being presented to the user.
+        /// </summary>
         public bool IsPresenting { get; private set; }
 
+        /// <inheritdoc />
         public void Present
             (IPreferencesDialogView view)
             {
             Present(view, null);
             }
 
+        /// <inheritdoc />
         public void Present
             (IPreferencesDialogView view,
              object parent)
@@ -51,7 +60,13 @@ namespace MouseNet.Logophi.Views.Presentation
                 }
             }
 
+        /// <summary>
+        /// Occurs when the delete cache button is clicked.
+        /// </summary>
         public event EventHandler DeleteCacheClicked;
+        /// <summary>
+        /// Occurs when the delete history button is clicked.
+        /// </summary>
         public event EventHandler DeleteHistoryClicked;
     }
 }
