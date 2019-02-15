9d3501b82d4675c4eeb58cc90c90ffde2c714bcf
mousebyte <ateague063@gmail.com>
2019-02-15 16:34:03 -0500

    Created a new settings helper and moved some functionality back into AppContext to more evenly divide responsibilities.

--- PresentationAgent.cs
+++ PresentationAgent.cs
@@ -1,124 +1,52 @@
 ﻿using System;
-using System.Windows.Forms;
-using MouseNet.Logophi.Forms;
-using MouseNet.Logophi.Views.Presentation;
 using MouseNet.Logophi.Thesaurus;
+using MouseNet.Logophi.Views;
+using MouseNet.Logophi.Views.Presentation;
 
 namespace MouseNet.Logophi
 {
     /// <inheritdoc />
     /// <summary>
-    /// Manages presentation objects.
+    ///     Manages presentation objects.
     /// </summary>
     internal class PresentationAgent : IDisposable
     {
-        private readonly BookmarksFormPresenter
-            _bookmarksFormPresenter;
-
-        private readonly MainFormPresenter _mainFormPresenter;
-
-        private readonly PreferencesDialogPresenter
-            _preferencesDialogPresenter;
-
-        private readonly Browser _thesaurus;
-
         public PresentationAgent
-            (Browser thesaurus)
+            (Browser browser,
+             Action exitAction,
+             Action showBookmarksAction,
+             Action showPreferencesAction,
+             Action showAboutAction,
+             Action deleteCacheAction,
+             Action deleteHistoryAction)
             {
-            _thesaurus = thesaurus;
-            //create presenters and hook up event handlers
-            _mainFormPresenter = new MainFormPresenter(
-                thesaurus,
-                Application.Exit,
-                PresentBookmarksForm,
-                PresentPreferencesForm,
-                PresentAboutDialog);
-            _bookmarksFormPresenter =
-                new BookmarksFormPresenter(thesaurus, _mainFormPresenter.Search);
-            _preferencesDialogPresenter =
-                new PreferencesDialogPresenter(DeleteCache, DeleteHistory);
+            var main = new MainFormPresenter(
+                browser,
+                exitAction,
+                showBookmarksAction,
+                showPreferencesAction,
+                showAboutAction);
+            Main = main;
+            Bookmarks =
+                new BookmarksFormPresenter(browser, main.Search);
+            Preferences =
+                new PreferencesDialogPresenter(
+                    deleteCacheAction,
+                    deleteHistoryAction);
             }
 
-        public event EventHandler PreferencesSaved;
-        
-        private void InvokePreferencesSaved
-            (object sender,
-             EventArgs args)
-            {
-            PreferencesSaved?.Invoke(sender, args);
-            }
+        public IViewPresenter<IMainFormView> Main { get; }
+        public IViewPresenter<IBookmarksFormView> Bookmarks { get; }
+        public IViewPresenter<IPreferencesDialogView> Preferences {
+            get;
+        }
 
         /// <inheritdoc />
         public void Dispose()
             {
-            _mainFormPresenter?.Dispose();
-            _bookmarksFormPresenter?.Dispose();
-            _preferencesDialogPresenter?.Dispose();
-            }
-
-        /// <summary>
-        /// Closes the Logophi main window.
-        /// </summary>
-        public void CloseMainForm()
-            {
-            if (_mainFormPresenter.IsPresenting)
-                _mainFormPresenter.View.Close();
-            }
-
-        /// <summary>
-        /// Presents the about window to the user.
-        /// </summary>
-        private void PresentAboutDialog()
-            {
-            var dialog = new AboutForm();
-            dialog.ShowDialog((IWin32Window) _mainFormPresenter.View);
-            dialog.Dispose();
-            }
-
-        /// <summary>
-        /// Presents the bookmarks window to the user.
-        /// </summary>
-        private void PresentBookmarksForm()
-            {
-            _bookmarksFormPresenter.Present(
-                new BookmarksForm(),
-                _mainFormPresenter.View);
-            }
-
-        /// <summary>
-        /// Presents the Logophi main window to the user, or
-        /// brings it to the front if it is already open.
-        /// </summary>
-        public void PresentMainForm()
-            {
-            if (_mainFormPresenter.IsPresenting)
-                _mainFormPresenter.View.ToFront();
-            else _mainFormPresenter.Present(new MainForm());
-            }
-
-        /// <summary>
-        /// Presents the preferences window to the user.
-        /// </summary>
-        private void PresentPreferencesForm()
-            {
-            var dialog = new PreferencesForm();
-
-            _preferencesDialogPresenter.Present(
-                dialog,
-                _mainFormPresenter.View);
-            dialog.Dispose();
-            //notify listeners that preferences were saved
-            InvokePreferencesSaved(this, EventArgs.Empty);
-            }
-
-        private void DeleteCache()
-            {
-            _thesaurus.ClearCache();
-            }
-
-        private void DeleteHistory()
-            {
-            _thesaurus.History.Clear();
+            Main.Dispose();
+            Bookmarks.Dispose();
+            Preferences.Dispose();
             }
     }
 }
---
+++ SettingsHelper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using MouseNet.Logophi.Properties;
+using MouseNet.Logophi.Thesaurus;
+using MouseNet.Logophi.Utilities;
+
+namespace MouseNet.Logophi
+{
+    internal class SettingsHelper : IDisposable
+    {
+        private readonly Browser _browser;
+        private readonly Settings _settings;
+        private Keys _registeredHotkey = Keys.None;
+
+        public SettingsHelper
+            (Settings settings,
+             Browser browser)
+            {
+            _settings = settings;
+            _browser = browser;
+            _settings.PropertyChanged += OnSettingsPropertyChanged;
+            }
+
+        public GlobalHotkey Hotkey { get; } = new GlobalHotkey();
+
+        public void Dispose()
+            {
+            Hotkey.Dispose();
+            }
+
+        public void UpdatePreferences()
+            {
+            //update hotkey registration
+            if (_settings.EnableHotkey)
+                RegisterHotkey();
+            else UnregisterHotkey();
+
+            //update autorun registry entry
+            var key = OpenAutoRunKey();
+            if (key == null) return;
+            if (_settings.AutoRun)
+                key.SetValue(Resources.AppName,
+                             Application.ExecutablePath);
+            else if (key.GetValue(Resources.AppName) != null)
+                key.DeleteValue(Resources.AppName);
+            }
+
+        /// <summary>
+        ///     Registers the hotkey stored in settings, or updates
+        ///     the currently registered hotkey if it is different than
+        ///     the one stored in settings.
+        /// </summary>
+        private void RegisterHotkey()
+            {
+            //return if no hotkey is set, or if the value in settings
+            //is the same as the hotkey already registered
+            if (_settings.Hotkey == Keys.None
+             || _settings.Hotkey == _registeredHotkey)
+                return;
+            if (_registeredHotkey != Keys.None)
+                Hotkey.UnregisterHotkey(0);
+            Hotkey.RegisterHotkey(_settings.Hotkey);
+            _registeredHotkey = _settings.Hotkey;
+            }
+
+        /// <summary>
+        ///     Unregisters the global hotkey if one is set.
+        /// </summary>
+        private void UnregisterHotkey()
+            {
+            if (_registeredHotkey == Keys.None) return;
+            Hotkey.UnregisterHotkey(0);
+            _registeredHotkey = Keys.None;
+            }
+
+        private void OnSettingsPropertyChanged
+            (object sender,
+             PropertyChangedEventArgs e)
+            {
+            //update browser property values when corresponding
+            //settings values are changed.
+            switch (e.PropertyName)
+                {
+                case "PersistentCache":
+                    _browser.PersistentCache =
+                        _settings.PersistentCache;
+                    break;
+                case "SaveHistory":
+                    _browser.History.PersistentHistory =
+                        _settings.SaveHistory;
+                    break;
+                case "MaxHistory":
+                    _browser.History.MaxItems =
+                        (int) _settings.MaxHistory;
+                    break;
+                }
+            }
+
+        /// <summary>
+        ///     Opens the Logophi autorun registry key.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="RegistryKey" /> that allows
+        ///     logophi to be run at logon.
+        /// </returns>
+        private static RegistryKey OpenAutoRunKey()
+            {
+            return Registry.CurrentUser.OpenSubKey(
+                Resources.AutoRunKey,
+                true);
+            }
+    }
+}
